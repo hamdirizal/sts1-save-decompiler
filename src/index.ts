d8821c7e7bf4889ac09c7f0eb1ffd430ee4602ad
Hamdi Rizal <hamdirizal@gmail.com>
2026-04-18 20:19:33 +0700

    Updated package.json

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
-import {
-  getFormattedToday,
-  getFormattedFutureDate,
-} from "./helpers/date-helper";
-
 function main(): void {
   console.log("Hello world from Bun + TypeScript");
-  console.log("Current date:", getFormattedToday());
-  console.log("Date 7 days from now:", getFormattedFutureDate(7));
 }
 
 main();
